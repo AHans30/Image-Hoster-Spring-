8a4280cf5370d90e30b48982ead60b8acba0d316
Abhimanyu Hans <abhimanyu.hans30@gmail.com>
2020-10-25 18:28:53 +0530

    add comment feature

--- src/main/java/ImageHoster/service/ImageService.java
+++ src/main/java/ImageHoster/service/ImageService.java
@@ -56,6 +56,6 @@ public class ImageService {
     //Returns true if logged user is owner of image to be edited/deleted
     //Returns false otherwise
     public boolean isLoggedUserImageOwner(User loggedUser, Image image) {
-        return loggedUser.getId() == image.getUser().getId();
+        return loggedUser.getId().equals(image.getUser().getId());
     }
 }
